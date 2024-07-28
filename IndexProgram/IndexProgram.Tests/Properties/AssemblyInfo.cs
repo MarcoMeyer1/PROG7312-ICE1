b6592c73ec4e49f6a591ee54b3a5627d782ac6bb
MarcoMeyer1 <marcomeyerbusiness@gmail.com>
2024-07-28 14:31:33 +0200

    added project files

---
+++ IndexProgram/IndexProgram.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IndexProgram.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IndexProgram.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0e13bd91-32e5-4402-910e-a97ba3b71218")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IndexProgram/IndexProgram.Tests/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IndexProject;
+using System;
+using System.IO;
+using IndexProgram;
+
+namespace IndexProject.Tests
+{
+    [TestClass]
+    public class IndexTests
+    {
+        [TestMethod]
+        public void TestIndexSetAndGet()
+        {
+            var index = new Index(5);
+            index[0] = 10;
+            Assert.AreEqual(10, index[0]);
+        }
+
+        [TestMethod]
+        public void TestIndexOutOfRange()
+        {
+            var index = new Index(5);
+            Assert.ThrowsException<IndexOutOfRangeException>(() => index[5] = 10);
+        }
+
+        [TestMethod]
+        public void TestDisplay()
+        {
+            var index = new Index(3);
+            index[0] = 1;
+            index[1] = 2;
+            index[2] = 3;
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                index.Display();
+                var result = sw.ToString().Trim();
+                Assert.AreEqual("1\r\n2\r\n3", result);
+            }
+        }
+
+        [TestMethod]
+        public void TestInitialValues()
+        {
+            var index = new Index(4);
+            for (int i = 0; i < 4; i++)
+            {
+                Assert.AreEqual(0, index[i], $"Initial value at index {i} should be 0.");
+            }
+        }
+
+        [TestMethod]
+        public void TestUpdateValues()
+        {
+            var index = new Index(3);
+            index[0] = 5;
+            index[1] = 10;
+            index[2] = 15;
+
+            Assert.AreEqual(5, index[0]);
+            Assert.AreEqual(10, index[1]);
+            Assert.AreEqual(15, index[2]);
+        }
+    }
+}
